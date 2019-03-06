5c990cd77094f69a99577202edf17cc843f8254a
Param Jain <param_jain@icloud.com>
2019-03-06 20:31:13 +0530

    Notices UI Changed

--- bunksheet-ean/src/Components/NoticeDetail.js
+++ bunksheet-ean/src/Components/NoticeDetail.js
@@ -14,7 +14,7 @@ export default NoticeDetail = ({ notice }) => {
             </View>
             <View style={{flexDirection: 'row', justifyContent:'center', alignContent: 'center', marginTop: 7}}>
                 <Text style={{flex: 4, borderWidth: 1, borderColor: '#eee', borderRadius: 10, marginHorizontal: 2, padding: 15, color: '#666'}}>{timestamp}</Text>
-                <TouchableOpacity style={{flex:1, borderWidth: 1, borderColor: '#666', borderRadius: 0, marginHorizontal: 2, padding: 15, justifyContent: 'center', alignContent: 'center'}} onPress={()=>{ Linking.openURL(ROOT_URL+filelink)}} >
+                <TouchableOpacity style={{flex:1, borderWidth: 1, borderColor: '#666', borderColor:'#FF5722', borderRadius: 30, marginHorizontal: 2, padding: 15, justifyContent: 'center', alignContent: 'center'}} onPress={()=>{ Linking.openURL(ROOT_URL+filelink)}} >
                 <Icon name='file-download' size={24} type='material' color = '#FF9800'/>
                 </TouchableOpacity>
                 <Text style={{flex: 4, borderWidth: 1, borderColor: '#eee', borderRadius: 10, marginHorizontal: 2, padding: 15, color: '#666'}}>{tname}</Text>
